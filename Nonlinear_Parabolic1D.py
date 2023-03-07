42a254a36b076a94183b92fbe9d13f8c66209802
Larrot <93649065+Larrot@users.noreply.github.com>
2023-03-07 20:29:05 +0500

    Add files via upload

--- Nonlinear_Parabolic1D.py
+++ Nonlinear_Parabolic1D.py
@@ -92,22 +92,24 @@ def Nonlinear_parabolic1D(D, Q, u_init, u_left, u_right, t_end, L, tau, N):
     c = np.zeros(N-3)
 
     while t < t_end:
-        # Filling RSE
-        for i in range(1, N-3):
-            RP[i] = u[i+1]+Q(u[i+1])*tau
-        RP[0] = u[1]+tau*Q(u[1])+C/2*(D(u[1])+D(u[0]))*u_left(0, t+tau)
-        RP[N-3] = u[N-2]+tau*Q(x[N-2])+C/2*(D(u[N-1]) +
-                                            D(u[N-2]))*u_right(L, t+tau)
-        # Filling lower diagonal
-        for i in range(N-3):
-            a[i] = -C/2*(D(u[i+2])+D(u[i+1]))
-        # Filling middle diagonal
-        for i in range(N-2):
-            b[i] = 1 + C/2*(D(u[i+2])+2*D(u[i+1])+D(u[i]))
-        # Filling upper diagonal
-        for i in range(N-3):
-            c[i] = -C/2*(D(u[i+2])+D(u[i+1]))
-        # Solving system of equations
-        u[1:N-1] = Tridiagonal_solver(a, b, c, RP)
+        for k in range(10):
+            # Filling RSE
+            for i in range(1, N-3):
+                RP[i] = u[i+1]+Q(u[i+1])*tau
+            RP[0] = u[1]+tau*Q(u[1])+C/2*(D(u[1])+D(u[0]))*u_left(0, t+tau)
+            RP[N-3] = u[N-2]+tau*Q(x[N-2])+C/2*(D(u[N-1]) +
+                                                D(u[N-2]))*u_right(L, t+tau)
+            # Filling lower diagonal
+            for i in range(N-3):
+                a[i] = -C/2*(D(u[i+2])+D(u[i+1]))
+            # Filling middle diagonal
+            for i in range(N-2):
+                b[i] = 1 + C/2*(D(u[i+2])+2*D(u[i+1])+D(u[i]))
+            # Filling upper diagonal
+            for i in range(N-3):
+                c[i] = -C/2*(D(u[i+2])+D(u[i+1]))
+            # Solving system of equations
+            u[1:N-1] = Tridiagonal_solver(a, b, c, RP)
+            k=k+1
         t += tau
     return u
